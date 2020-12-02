4eaa55eb732823fefeb49f1e42ae090811bd3b38
Franco Toshiro Romero Morales <i201913000@cibertec.edu.pe>
2020-12-01 20:18:38 -0500

    Enviado

--- Dominio.MainModule/DistrtitoManager.cs
+++ Dominio.MainModule/DistrtitoManager.cs
@@ -1,10 +1,6 @@
 ﻿using Dominio.Core.Entities;
 using Infraestructura.Data.SqlServer;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio.MainModule
 {
